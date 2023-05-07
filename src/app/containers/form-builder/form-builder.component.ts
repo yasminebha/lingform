ac5dff0fffd8844eb7035d51b262c6e868bb84b0
yasminebha <ybelhajali@gmail.com>
2023-05-08 00:17:11 +0100

    add formbuilder that contains all form elements

---
+++ src/app/containers/form-builder/form-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lg-form-builder',
+  templateUrl: './form-builder.component.html',
+  styleUrls: ['./form-builder.component.css']
+})
+export class FormBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
