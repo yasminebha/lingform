6965b391fc59b565979df9c42d00c59e5c22c44e
yasminebha <ybelhajali@gmail.com>
2023-05-08 00:24:02 +0100

    make textfield support form control

--- src/shared/ui-components/text-field/text-field.component.ts
+++ src/shared/ui-components/text-field/text-field.component.ts
@@ -1,25 +1,60 @@
-
-import { Component, OnInit,Input,ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  forwardRef,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'lg-text-field',
   templateUrl: './text-field.component.html',
-  styleUrls: ['./text-field.component.css']
+  styleUrls: ['./text-field.component.css'],
+  providers: [
+    {
+      multi: true,
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextFieldComponent),
+    },
+  ],
 })
-export class TextFieldComponent implements OnInit {
-  @ViewChild('input') inputElement!: ElementRef;
+export class TextFieldComponent implements OnInit, ControlValueAccessor {
 
-  constructor() { }
   @Input()
-  type : string='text';
+  type: string = 'text';
   @Input()
-  placeholder? : string ='';
+  label?: string = undefined;
   @Input()
-  layout: string ='text-field';
+  fontSize!: string;
   @Input()
-  isDisabled: boolean = false;
-  ngOnInit(): void {
-    
+  placeholder?: string = '';
+  @Input()
+  layout: string = 'text-field';
+  @Input()
+  value?: string = undefined;
+  @Input()
+  isDisabled:boolean=false
+
+  constructor(private $el: ElementRef<HTMLInputElement>) {}
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+    this.$el.nativeElement.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    if(isDisabled)
+    this.$el.nativeElement.disabled = isDisabled;
   }
 
+  ngOnInit(): void {}
 }
