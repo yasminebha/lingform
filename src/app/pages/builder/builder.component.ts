3ed0a1a9d42b593d48d6bac37d11f8022c20b077
yasminebha <ybelhajali@gmail.com>
2023-05-10 19:04:35 +0100

    add builder module

---
+++ src/app/pages/builder/builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lg-builder',
+  templateUrl: './builder.component.html',
+  styleUrls: ['./builder.component.css']
+})
+export class BuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/builder/builder.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BuilderRoutingModule } from './builder-routing.module';
+import { BuilderComponent } from '../builder/builder.component';
+import { FormBuilderComponent } from '@/app/containers/form-builder/form-builder.component';
+import { LeftSideBarComponent } from '@/app/containers/left-side-bar/left-side-bar.component';
+import { RightSideBarComponent } from '@/app/containers/right-side-bar/right-side-bar.component';
+import { UIComponentsModule } from '@/shared/ui-components/ui-components.module';
+import { InputComponentsComponent } from '@/app/components/input-components/input-components.component';
+import { HeadingElementComponent } from '@/app/components/heading-element/heading-element.component';
+import { MultipleChoiceElementComponent } from '@/app/components/multiple-choice-element/multiple-choice-element.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ShortAnswerComponent } from '@/app/components/short-answer/short-answer.component';
+import { OneChoiceComponent } from '@/app/components/one-choice/one-choice.component';
+import { EmailElementComponent } from '../../components/email-element/email-element.component';
+import { PhoneElementComponent } from '../../components/phone-element/phone-element.component';
+
+
+@NgModule({
+  declarations: [
+    BuilderComponent,
+    RightSideBarComponent,
+    LeftSideBarComponent,
+    FormBuilderComponent,
+    InputComponentsComponent,
+    HeadingElementComponent,
+    MultipleChoiceElementComponent,
+    ShortAnswerComponent,
+    OneChoiceComponent,
+    EmailElementComponent,
+    PhoneElementComponent,
+  ],
+  imports: [
+    
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BuilderRoutingModule,
+    UIComponentsModule,
+  ]
+})
+export class BuilderModule { }
