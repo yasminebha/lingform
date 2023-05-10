0fa7c70767863a6a94123a685121b502a2091ef9
yasminebha <ybelhajali@gmail.com>
2023-05-10 19:00:54 +0100

    add multiple choice element component

--- src/app/components/multiple-choice-element/multiple-choice-element.component.ts
+++ src/app/components/multiple-choice-element/multiple-choice-element.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { checkboxList } from '@/shared/mock-data/multiple-choice';
+import * as shortid from 'shortid';
+export type MultipleChoiceOption = { key: string; value: string };
 @Component({
   selector: 'lg-multiple-choice-element',
   templateUrl: './multiple-choice-element.component.html',
-  styleUrls: ['./multiple-choice-element.component.css']
+  styleUrls: ['./multiple-choice-element.component.css'],
 })
 export class MultipleChoiceElementComponent implements OnInit {
-  mode: 'edit' | 'live' = 'live';
+  mode: 'edit' | 'live' = 'edit';
   checkboxOptions = checkboxList;
-  constructor() { }
+  options: MultipleChoiceOption[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.checkboxOptions=checkboxList;
+    this.checkboxOptions = checkboxList;
     console.log(this.checkboxOptions);
-    
+    if (this.mode === 'edit') {
+      this.addChoice();
+      this.addChoice();
+    }
   }
-  onClick(){
-    console.log('works')
+  onClick() {
+    console.log('works');
   }
+  addChoice() {
+    this.options.push(
+      { key: shortid.generate(), value: '' }
+  
+    );
+  }
+  removeChoice(key: string) {
+    if (this.options.length > 2)
+      this.options = this.options.filter((opt) => opt.key !== key);
+  }
+  updateChoice(event: any, key: string) {
+    const value = event.target.value;
 
+    const obj = this.options.find((opt) => opt.key === key);
+    if (obj) {
+      obj.value = value;
+    }
+    console.log(this.options);
+  }
 }
