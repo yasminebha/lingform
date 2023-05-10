62cd7d88f579da4bdd80579c06aa4a059db584d5
yasminebha <ybelhajali@gmail.com>
2023-05-10 19:02:36 +0100

    add one choice element component

---
+++ src/app/components/one-choice/one-choice.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MultipleChoiceElementComponent } from '../multiple-choice-element/multiple-choice-element.component';
+
+@Component({
+  selector: 'lg-one-choice',
+  templateUrl: './one-choice.component.html',
+  styleUrls: ['./one-choice.component.css','../multiple-choice-element/multiple-choice-element.component.css']
+})
+export class OneChoiceComponent extends MultipleChoiceElementComponent implements OnInit  {
+ 
+
+  constructor() {
+    super();
+  }
+
+  override ngOnInit(): void {
+    this.addChoice()
+    this.addChoice()
+  }
+
+}
