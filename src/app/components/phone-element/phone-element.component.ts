6e22d27aa9cb6c1823216c4291369256bfeabc06
yasminebha <ybelhajali@gmail.com>
2024-05-20 16:20:16 +0100

    add phone input

--- src/app/components/phone-element/phone-element.component.ts
+++ src/app/components/phone-element/phone-element.component.ts
@@ -4,7 +4,8 @@ import { ShortAnswerComponent } from '../short-answer/short-answer.component';
 @Component({
   selector: 'lg-phone-element',
   templateUrl: './phone-element.component.html',
-  styleUrls: ['./phone-element.component.css'],
+  styleUrls: ['./phone-element.component.css',
+  '../short-answer/short-answer.component.css',],
 })
 export class PhoneElementComponent
   extends ShortAnswerComponent
