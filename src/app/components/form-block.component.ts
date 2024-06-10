86f6fc66341ffb531d2b26209f9cc7f648ce5e4c
yasminebha <ybelhajali@gmail.com>
2024-06-10 17:00:54 +0100

    add block duplication feature

--- src/app/components/form-block.component.ts
+++ src/app/components/form-block.component.ts
@@ -3,7 +3,9 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../store/reducers';
 import { BaseControlComponent } from '@/shared/ui-components/base-control.component';
 import { QuestionService } from '@/shared/services/question.service';
-import { removeBlock, updateBlockOrder } from '../store/actions/builder.actions';
+import { addBlock, removeBlock, updateBlockOrder } from '../store/actions/builder.actions';
+import * as shortid from 'shortid';
+import { QuestionElement } from '@/shared/models/questionElement.model';
 
 @Component({
   template: '',
@@ -45,4 +47,29 @@ export class FormBlockComponent<TValue, TMeta=any>
     this.store.dispatch(removeBlock({ blockId }));
 
   }
+  duplicateBlock() {
+
+    
+    const newBlockId = shortid.generate();
+    let form_id: string | null = '';
+    let blockOrder:string[]= []
+    this.store
+      .select((state) => state.builder)
+      .subscribe((builder) => {
+        form_id = builder.form_id;
+        blockOrder = [...builder.blockOrder];
+      }).unsubscribe();
+    const newBlock: QuestionElement = {
+      form_id: form_id,
+      kind: this.kind,
+      questLabel: this.label,
+      quest_id: newBlockId,
+      required: this.required,
+      quest_meta: { ...this.metaData },
+    };
+    this.store.dispatch(addBlock({ blockId: newBlockId, newBlock }));
+    blockOrder.push(newBlockId);
+    this.store.dispatch(updateBlockOrder({ blockOrder }));
+  }
 }
+
