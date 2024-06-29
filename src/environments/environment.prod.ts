cd77b60bdd7e881a2df65fcd044703c36aa843dc
yasminebha <98106066+yasminebha@users.noreply.github.com>
2024-06-29 11:27:35 +0000

    deployement  fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  supabase: {
+    url: "https://vkzvjnihvrpagqqkgvtr.supabase.co",
+    storageUrl:"",
+    apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrenZqbmlodnJwYWdxcWtndnRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5NTI3MzQsImV4cCI6MTk5ODUyODczNH0.2tSDvuXKpdiCgiqvGb1V5i-ErF6rhEXCbRIQeH8zLsg"
+  },
 };
