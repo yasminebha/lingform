513cd7e6882dd05f12965df4c6ec6666bb0dadbb
yasminebha <98106066+yasminebha@users.noreply.github.com>
2024-07-07 13:10:47 +0000

    fix number slider functionality

--- src/shared/ui-components/number-slider/number-slider.component.ts
+++ src/shared/ui-components/number-slider/number-slider.component.ts
@@ -1,7 +1,9 @@
 import { updateBlock } from '@/app/store/actions/builder.actions';
 import { AppState } from '@/app/store/reducers';
+import { QuestionElement } from '@/shared/models/questionElement.model';
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'lg-number-slider',
@@ -9,30 +11,37 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./number-slider.component.css']
 })
 export class NumberSliderComponent implements OnInit {
-  @Input() blockId!: string;
+  @Input() block$!: BehaviorSubject<QuestionElement | null>;
+  block: QuestionElement | null = null;
   @Input() maxRangeNumber: number = 10;
   maxFile: number = 1;
+  private subscription!: Subscription;
+  
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store
-      .select((state) => state.builder)
-      .subscribe(({ blocks }) => {
-        this.maxFile = blocks[this.blockId]?.quest_meta?.maxFileNumber || 1;
-      })
-      .unsubscribe();
+    this.subscription = this.block$.subscribe(block => {
+      this.block = block;
+    });
   }
 
   onSliderChange(evt: any): void {
     this.maxFile = evt.target.value;
+    if(this.block)
     this.store.dispatch(
       updateBlock({
-        blockId: this.blockId,
+        blockId: this.block.quest_id,
         quest_meta: {
           maxFileNumber: this.maxFile,
         },
       })
     );
+
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
