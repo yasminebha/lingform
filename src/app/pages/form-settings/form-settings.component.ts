3852dcaab4b5750b9c6502d813e98b5a021e8502
yasminebha <ybelhajali@gmail.com>
2024-09-03 19:47:43 +0100

    display the formView according to the form settings (not fixed)

--- src/app/pages/form-settings/form-settings.component.ts
+++ src/app/pages/form-settings/form-settings.component.ts
@@ -6,6 +6,7 @@ import { AppState } from '@/app/store/reducers';
 import { distinctUntilChanged, Subscription } from 'rxjs';
 import { FormService } from '@/shared/services/form.service';
 import { Form } from '@/shared/models/form.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form-settings',
@@ -23,17 +24,32 @@ export class FormSettingsComponent implements OnInit, OnDestroy {
   storeSubscription = new Subscription();
   builderState: any;
 
-  constructor(private store: Store<AppState>, private fs: FormService) {}
+  constructor(private store: Store<AppState>, private fs: FormService, private readonly route: ActivatedRoute,
+  ) {}
 
-  ngOnInit(): void { 
+  async ngOnInit(): Promise<void> { 
+    const formId = this.route.snapshot.paramMap.get('id');
+
     this.storeSubscription = this.store
       .select((state) => state.builder)
       .pipe(distinctUntilChanged())
       .subscribe(async (builderState) => {
         this.settings = { ...builderState.settings };
-
         this.fs.autoSave(builderState);
       });
+      if(formId)
+      this.form = await this.fs.getFormById(formId);
+      if(this.form){
+        this.store.dispatch(
+          updateBuilder({
+            form_id: this.form!.form_id,
+            settings:this.form?.settings||{}
+            
+          })
+        );
+      }
+
+
   }
 
   ngOnDestroy(): void {
--- src/app/pages/view/view.component.ts
+++ src/app/pages/view/view.component.ts
@@ -1,9 +1,12 @@
 import { updateBuilder } from '@/app/store/actions/builder.actions';
 import { AppState } from '@/app/store/reducers';
-'@/shared/services/form.service';
-import { Component,  OnInit } from '@angular/core';
-
-import { Store } from '@ngrx/store';
+import { Form } from '@/shared/models/form.model';
+import { FormService } from '@/shared/services/form.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { State, Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view',
@@ -11,18 +14,35 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
- 
-  mode:'live'|'edit'='live'
-  constructor(
-  private store:Store<AppState>
-  ) {}
-
+  mode: 'live' | 'edit' = 'live';
+  shouldDisplayForm$!: Observable<boolean>;
+  settings:any
+  constructor(private store: Store<AppState>,private route:ActivatedRoute,private fs:FormService) {}
+  storeSubscription = new Subscription();
+form!:Form
   async ngOnInit(): Promise<void> {
-      this.store.dispatch(updateBuilder({ 
-        mode:this.mode
-      }));
+    const formId = this.route.snapshot.paramMap.get('id')
+   
+      if(formId)
+      this.form = await this.fs.getFormById(formId);
+      if(this.form){
+       this.settings= this.form.settings
+      }
+   
+    this.store.dispatch(updateBuilder({ mode: this.mode }));
+    console.log(this.settings);
     
   }
 
+  private shouldDisplayForm(settings: any): boolean {
+    const currentTime = new Date();
+    const openTime = new Date(settings.openTime);
+    const closeTime = new Date(settings.closeTime);
 
+    return (
+      settings.acceptResponses &&
+      (!settings.openTime || currentTime >= openTime) &&
+      (!settings.closeTime || currentTime <= closeTime)
+    );
+  }
 }
